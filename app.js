d9e19918353caeaf5ac23455535d746901171b61
Candas Kilic <candas.kilic@outlook.com>
2022-05-17 21:10:50 +0200

    express added

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const express = require("express");
-
-const server = http.createServer((req, res) => {
-  res.end("First response...");
-});
-
-server.listen(process.env.PORT || 3000);
