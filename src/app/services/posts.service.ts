13d5a83d975babda84bd7f57b4cba03baa8fd911
Candas Kilic <candas.kilic@outlook.com>
2022-05-27 11:23:23 +0200

    Request updates. Delete performed in the frontend side

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -45,7 +45,6 @@ export class PostsService {
 
     this.http.post<{ message: string }>(this.url, newPost)
       .subscribe((data) => {
-        console.log(data.message);
         this.posts.push(newPost);
         this.postsUpdated.next([...this.posts]);
       });
@@ -54,7 +53,9 @@ export class PostsService {
   deletePost(id: string): void {
     this.http.delete<{ message: string }>(`${this.url}/${id}`)
       .subscribe((data) => {
-        console.log(data);
+        const updatedPosts = this.posts.filter(post => post.id !== id);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 }
